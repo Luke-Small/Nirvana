19b42a1e2d945e168738050e0d9d00acf68a620e
Luke Small <lsmal004@odu.edu>
2024-12-01 15:25:02 -0500

    Initial Commit

---
+++ Nirvana/NirvanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NirvanaApp.swift
+//  Nirvana
+//
+//  Created by Luke S on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NirvanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NirvanaTests/NirvanaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NirvanaTests.swift
+//  NirvanaTests
+//
+//  Created by Luke S on 12/1/24.
+//
+
+import Testing
+@testable import Nirvana
+
+struct NirvanaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
